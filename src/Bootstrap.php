27143220f8879aea409e3b82def0ba47661dbac6
Aydan Thompson <aydan.thompson@bt.com>
2024-11-28 16:00:36 +0000

    Added error handler

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -2,4 +2,22 @@
 
 declare(strict_types=1);
 
-echo 'Hello World!';
+namespace CarltonHonda;
+
+require __DIR__ . "/../vendor/autoload.php";
+
+error_reporting(E_ALL);
+
+$environment = "development";
+
+$whoops = new \Whoops\Run;
+if ($environment !== "production") {
+  $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+} else {
+  $whoops->pushHandler(function ($e) {
+    echo "TOD0: User friendly error page and email functionality.";
+  });
+}
+$whoops->register();
+
+throw new \Exception;
